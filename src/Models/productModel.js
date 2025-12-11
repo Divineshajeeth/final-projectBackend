eb48885d5254dce4c0d1ca104aa8aeb912b20852
shajeeth <shajeethshajeeth5@gmail.com>
2025-12-11 11:49:24 +0530

    change01

--- src/Models/productModel.js
+++ src/Models/productModel.js
@@ -7,7 +7,7 @@ const productSchema = new mongoose.Schema(
     name: { type: String, required: true },
     description: { type: String },
     price: { type: Number, required: true },
-    currency: { type: String, default: "LKR", enum: ["LKR", "USD", "EUR", "INR"] },
+    currency: { type: String, default: "USD", enum: ["LKR", "USD", "EUR", "INR"] },
     size: { type: String },
     unit: { type: String }, // Added to match frontend 'unit' field
     image: { type: String }, // Added to store image path/URL
